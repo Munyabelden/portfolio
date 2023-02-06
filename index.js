56dbbf6eca0254608b0b7060d52dcf0f73a5fb19
munyamugb <munyaradzi045@gmail.com>
2023-02-06 18:27:18 +0200

    Add eslint

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const navBar = document.getElementById('nav-bar');
-const menuBtn = document.getElementById('menu-btn');
-const exitBtn =document.getElementById('exit-btn');
+const navBar = document.querySelector('#nav-bar');
+const menuBtn = document.querySelector('#menu-btn');
+const exitBtn = document.querySelector('#exit-btn');
+const links = document.querySelectorAll('.nav-item');
+    
+
 
 const menuActivity = () => {
-
     const visibility = navBar.getAttribute('data-visible');
-
-    if(visibility === "false"){
-        navBar.setAttribute("data-visible", true)
-    }else if(visibility === "true"){
-        navBar.setAttribute("data-visible", false)
+    if(visibility === 'false'){
+        navBar.setAttribute('data-visible', true)
+    }else if(visibility === 'true'){
+        navBar.setAttribute('data-visible', false)
     }
 }
 
 menuBtn.addEventListener('click', menuActivity );
 exitBtn.addEventListener('click', menuActivity );
+
+links.forEach( link => {
+    const visibility = navBar.getAttribute('data-visible');
+    link.addEventListener('click', () => {
+        navBar.setAttribute('data-visible', false)
+    })
+})
