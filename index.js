5a814ff7e08a720346c51eeb6b48fbf2dacebba8
munyamugb <munyaradzi045@gmail.com>
2023-02-07 15:05:40 +0200

    Add pop page style

--- index.js
+++ index.js
@@ -14,12 +14,7 @@ popUpCard.forEach(button => {
     card.classList.add('active')
  }
 
- closeCard.forEach(button => {
-    button.addEventListener('click', () => {
-      const card = button.closest('.card')
-      remove(card)
-    })
-  })
+
 
  function remove(card){
     if(card == null)return
