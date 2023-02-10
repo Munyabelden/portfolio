7d2c823e9c1540994183243c0494511ea9f4aa8f
munyamugb <munyaradzi045@gmail.com>
2023-02-10 14:38:50 +0200

    Localstorage js impliment

--- form.js
+++ form.js
@@ -16,3 +16,43 @@ form.addEventListener('submit', (event) => {
       email.classList.add('error');
     }
   });
+
+  const name = document.querySelector('#name');
+
+  function storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return e instanceof DOMException && (
+        e.code === 22
+              || e.code === 1014
+              || e.name === 'QuotaExceededError'
+              || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+              && (storage && storage.length !== 0);
+    }
+  }
+
+  if (storageAvailable('localStorage')) {
+
+    const updateFormData = () => {
+
+      const formInput = {
+
+        name: form.name.value,
+        email: form.email.value,
+        message: form.message.value,
+
+      };
+  
+      localStorage.setItem('formInput', JSON.stringify(formInput));
+    };
+
+    form.name.addEventListener('change', updateFormData);
+    form.email.addEventListener('change', updateFormData);
+    form.message.addEventListener('change', updateFormData);
+  
